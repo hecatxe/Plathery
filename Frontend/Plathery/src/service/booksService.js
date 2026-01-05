d243d3f26274625c25501bc685ddf386efa06299
Fátima <116075109+hecatxe@users.noreply.github.com>
2026-01-05 17:20:58 +0100

    Update bookService.jsxj
    Variable del backend

--- Frontend/Plathery/src/service/booksService.js
+++ Frontend/Plathery/src/service/booksService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
-
+const API = import.meta.env.VITE_API_BACKEND_URL;
 
 // Servicio para obtener los bestsellers
 export const getBestSellersNYT = async (list = "hardcover-fiction") => {
@@ -13,15 +13,11 @@ export const getBestSellersNYT = async (list = "hardcover-fiction") => {
 };
 // Servicio que obtiene todos los libros
 export async function getBooks() {
-  const res = await fetch("http://localhost:3000/books", {
-    credentials: "include",
-  });
+  const res = await fetch(`${API}/books`, { credentials: "include" });
   return res.json();
 }
 // Servicio que obtiene un libro por su ID
 export async function getBookById(id) {
-  const res = await fetch(`http://localhost:3000/books/${id}`, {
-    credentials: "include",
-  });
+  const res = await fetch(`${API}/books/${id}`, { credentials: "include" });
   return res.json();
 }
